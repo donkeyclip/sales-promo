7e0c48f3f33edf67fb0fdab05baa47447bdb1c3b
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2022-04-14 11:46:05 +0300

    fix: update assets

--- clip/initParams.js
+++ clip/initParams.js
@@ -6,14 +6,14 @@ export const initParams = [
       secondaryColor:"#9975e0",
       backgroundColor:"#284AC7",
       smartwatchImg:
-        "https://donkeyclip.github.io/sales-promo/demo/images/smartwatch.png",
+        "https://donkeyclip.github.io/sales-promo/demo/assets/smartwatch.png",
       smartphoneBack:
-        "https://donkeyclip.github.io/sales-promo/demo/images/mobile-back.png",
+        "https://donkeyclip.github.io/sales-promo/demo/assets/mobile-back.png",
       smartphoneFront:
-        "https://donkeyclip.github.io/sales-promo/demo/images/mobile-front.png",
-      bandImg: "https://donkeyclip.github.io/sales-promo/demo/images/band.png",
+        "https://donkeyclip.github.io/sales-promo/demo/assets/mobile-front.png",
+      bandImg: "https://donkeyclip.github.io/sales-promo/demo/assets/band.png",
       shoppingBagIcon:
-        "https://donkeyclip.github.io/sales-promo/demo/images/shopping_bag_icon.png",
+        "https://donkeyclip.github.io/sales-promo/demo/assets/shopping_bag_icon.png",
     },
   },
 ];
